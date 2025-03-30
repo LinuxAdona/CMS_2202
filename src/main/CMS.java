fb0fa9524ca688f77843a40dbb9f76cebad188dc
Linux Adona <linuxadona17@gmail.com>
2025-03-30 23:46:29 +0800

    First Commit

---
+++ src/main/CMS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import ui.LoginFrame;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class CMS {
+
+    public static void main(String[] args) {
+        LoginFrame login = new LoginFrame();
+        login.setVisible(true);
+    }
+}
